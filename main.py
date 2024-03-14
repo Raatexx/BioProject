999ac226025dcd5b7e96246b7dc661b00cf11d3e
Ratex <prmorais2006@gmail.com>
2024-03-14 20:51:51 -0300

    Leituro do arquivo

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import Bio
+from Bio import SeqIO
 
 #setando caminho do arquivo
 
 caminhoDoArquivo = 'data/gene.fna'
 
+#realizando leitura do arquivo fna
 
+with open(caminhoDoArquivo, 'r') as arquivo:
+    for sequencia in SeqIO.parse(arquivo, 'fasta'):
+        
