f20f1c7b884296e19b3d02072cb1cc6339750d62
Ratex <prmorais2006@gmail.com>
2024-03-15 20:06:38 -0300

    Tornando o sequenciamento em string para trabalhar com ela

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from Bio import SeqIO
 caminhoDoArquivo = 'data/gene.fna'
 
 #realizando leitura do arquivo fna
-
+memoria = []
 with open(caminhoDoArquivo, 'r') as arquivo:
     for sequencia in SeqIO.parse(arquivo, 'fasta'):
-        
+        memoria.append(str(sequencia.seq))
+        print(memoria)
+
