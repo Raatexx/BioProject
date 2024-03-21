f494cf47123428f6eac58760fa5aa96d03de3ee8
Ratex <prmorais2006@gmail.com>
2024-03-21 10:19:48 -0300

    Adição do método de tradução

--- main.py
+++ main.py
@@ -28,6 +28,58 @@ def transcricao(nucleotideo):
         
     print(genomaTranscrito)
     
+rnaMensageiro = ['AUGAUUUCGUAA']  
+def traducao(rnaMensageiro):
+    sinteseProteica = []
+    for codon in rnaMensageiro:  # iterador para cada base nitrogenada
+        for x in range(3, (len(codon) + 3), 3): # iterador para dividir as bases de 3 em 3, formando os códons
+            if codon[x -3: x] == 'AUG': # condição para formação das proteínas
+                sinteseProteica.append('Met')
+            elif codon[x -3: x] == 'AUC' or codon[x -3: x] == 'AUU' or codon[x -3: x] == 'AUA':
+                sinteseProteica.append('Ile')
+            elif codon[x -3: x] == 'UCG' or codon[x -3: x] == 'UCA' or codon[x -3: x] == 'UCC' or codon[x -3: x] == 'UCU' or codon[x -3: x] == 'AGU' or codon[x -3: x] == 'AGC':
+                sinteseProteica.append('Ser')
+            elif codon[x -3: x] ==  'UUU' or  codon[x -3: x] == 'UUC':
+                sinteseProteica.append('Phe')
+            elif codon[x -3: x] == 'UUA' or codon[x -3: x] == 'UUG':
+                sinteseProteica.append('Leu')
+            elif codon[x -3: x] == 'GUU' or codon[x -3: x] == 'GUC' or codon[x -3: x] == 'GUA' or codon[x -3: x] == 'GUG':
+                sinteseProteica.append('Val')
+            elif codon[x -3: x] == 'CUU' or codon[x -3: x] == 'CCC' or codon[x -3: x] == 'CCA' or codon[x -3: x] == 'CCG':
+                sinteseProteica.append('Pro')
+            elif codon[x -3: x] == 'ACU' or codon[x -3: x] == 'ACC' or codon[x -3: x] == 'ACA' or codon[x -3: x] == 'ACG':
+                sinteseProteica.append('Thr')
+            elif codon[x -3: x] == 'GCU' or codon[x -3: x] == 'GCC' or codon[x -3: x] == 'GCA' or codon[x -3: x] == 'GCG':
+                sinteseProteica.append('Ala')
+            elif codon[x -3: x] == 'UAU' or codon[x -3: x] == 'UAC':
+                sinteseProteica.append('Tyr') 
+            elif codon[x -3: x] == 'CAU' or codon[x -3: x] == 'CAC':
+                sinteseProteica.append('His')
+            elif codon[x -3: x] == 'CAA' or codon[x -3: x] == 'CAG':
+                sinteseProteica.append('Gln')
+            elif codon[x -3: x] == 'AUU' or codon[x -3: x] == 'AAC':
+                sinteseProteica.append('Asn')
+            elif codon[x -3: x] == 'AAA' or codon[x -3: x] == 'AAG':
+                sinteseProteica.append('Lys')
+            elif codon[x -3: x] == 'GAU' or codon[x -3: x] == 'GAC':
+                sinteseProteica.append('Asp')
+            elif codon[x -3: x] == 'GAA' or codon[x -3: x] == 'GAG':
+                sinteseProteica.append('Glu')
+            elif codon[x -3: x] == 'UGU' or codon[x -3: x] == 'UGC':
+                sinteseProteica.append('Cys')
+            elif codon[x -3: x] == 'UGG':
+                sinteseProteica.append('Trp')
+            elif codon[x -3: x] == 'CGU' or codon[x -3: x] == 'GCC' or codon[x -3: x] == 'CGA' or codon[x -3: x] == 'CGG' or codon[x -3: x] == 'AGA' or codon[x -3: x] == 'AGG':
+                sinteseProteica.append('Arg')
+            elif codon[x -3: x] == 'GGU' or codon[x -3: x] == 'GGC' or codon[x -3: x] == 'GGA' or codon[x -3: x] == 'GGG':
+                sinteseProteica.append('Gly')
+            elif codon[x -3: x] == 'UAA' or codon[x -3: x] == 'UAG' or codon[x -3: x] == 'UGA':
+                sinteseProteica.append('Parada')
+    print(sinteseProteica)
+                
+traducao(rnaMensageiro)
+        
+    
                     
 
-transcricao(nucleotideo)
+#transcricao(nucleotideo)
